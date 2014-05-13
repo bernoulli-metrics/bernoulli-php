7236c7ff520558090ec60bcaf67a3f429cf42b48
Joe Gasiorek <joe.gasiorek@gmail.com>
2014-05-13 11:48:18 -0700

    first draft of a client

--- src/Bernoulli/Client.php
+++ src/Bernoulli/Client.php
@@ -1,3 +1,79 @@
 <?php
-
 namespace Bernoulli;
+
+use GuzzleHttp;
+
+class Bernoulli {
+    const URL = "https://bernoulli.herokuapp.com/client/api/experiments/";
+
+    static public function GetExperiments($experimentIds, $userId, $userData, $clientId)
+    {
+        $clientId = self::GetClientId($clientId);
+
+        $queryString = [
+            'clientId' => $clientId,
+            'experimentIds' => join(',', $experimentIds),
+            'userId' => $userId,
+        ];
+
+        if ($userData != null) {
+            $queryString = array_combine($queryString, $userData);
+        }
+
+        $httpClient = new GuzzleHttp\Client();
+        $response = $httpClient->get(self::URL, [
+            'query' => $queryString,
+        ]);
+
+        if ($response->getStatusCode() != 200) {
+            throw new \Exception("Unable to get experiments");
+        }
+
+        $data = $response->json();
+        if ($data['status'] != 'ok') {
+            throw new \Exception($data['message']);
+        }
+
+        return $data['value'];
+    }
+
+    static public function GoalAttained($experimentId, $userId, $clientId)
+    {
+        $clientId = self::GetClientId($clientId);
+
+        $queryString = [
+            'clientId' => $clientId,
+            'experimentId' => $experimentId,
+            'userId' => $userId,
+        ];
+
+        $httpClient = new GuzzleHttp\Client();
+        $response = $httpClient->post(self::URL, [
+            'query' => $queryString,
+
+        ]);
+
+        if ($response->getStatusCode() != 200) {
+            throw new \Exception("Unable to record goal");
+        }
+
+        $data = $response->json();
+        if ($data['status'] != 'ok') {
+            throw new \Exception($data['message']);
+        }
+
+        return $data['success'];
+    }
+
+    static private function GetClientId($clientId) {
+        if ($clientId == null || $clientId == "") {
+            $clientId = getenv("BERNOULLI_CLIENT_ID");
+
+            if ($clientId == null || $clientId == "" ){
+                throw new \Exception("Invalid $clientId");
+            }
+        }
+
+        return $clientId;
+    }
+}
